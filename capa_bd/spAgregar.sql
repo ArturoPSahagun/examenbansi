a377b43978f0cffc68546228623453bc8792c2b8
Arturo Sahagun <arturopbsahagun@gmail.com>
2023-07-17 18:26:06 -0600

    creado el script de creacion para spAgregar

---
+++ capa_bd/spAgregar.sql
@@ -0,0 +1,16 @@
+CREATE PROCEDURE spAgregar(
+	@nombre AS VARCHAR(255), 
+	@desc AS VARCHAR(255))
+AS
+BEGIN
+	BEGIN TRY
+		INSERT INTO tblExamen(Nombre, Descripcion)
+		VALUES (@nombre, @desc);
+		SELECT 0 AS codigo_retorno;
+	END TRY
+	BEGIN CATCH
+		SELECT  
+            ERROR_NUMBER() AS codigo_retorno,
+			ERROR_MESSAGE() AS mensaje_retorno
+	END CATCH
+END;
