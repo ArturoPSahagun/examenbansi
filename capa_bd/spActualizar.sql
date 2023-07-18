d5e7de442c5c53d9720c981321b7854bdc6d1dc0
Arturo Sahagun <arturopbsahagun@gmail.com>
2023-07-17 18:44:27 -0600

    Añadido script para crear spActualizar

---
+++ capa_bd/spActualizar.sql
@@ -0,0 +1,23 @@
+CREATE PROCEDURE spActualizar(
+	@id AS INT,
+	@nombre AS VARCHAR(255), 
+	@descripcion AS VARCHAR(255))
+AS
+BEGIN
+	BEGIN TRY
+		UPDATE tblExamen
+		SET 
+			Nombre = @nombre,
+			Descripcion = @descripcion
+		WHERE
+			idExamen = @id
+		SELECT 
+            0 AS codigo_retorno,
+            'Registro insertado satisfactoriamente' AS mensaje_retorno;
+	END TRY
+	BEGIN CATCH
+		SELECT  
+            ERROR_NUMBER() AS codigo_retorno,
+			ERROR_MESSAGE() AS mensaje_retorno
+	END CATCH
+END;
