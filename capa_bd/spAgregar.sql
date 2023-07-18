79644a7160ee35eef10bf886b2d3f227ac308578
Arturo Sahagun <arturopbsahagun@gmail.com>
2023-07-17 18:32:20 -0600

    Fixed typo

--- capa_bd/spAgregar.sql
+++ capa_bd/spAgregar.sql
@@ -1,14 +1,14 @@
 CREATE PROCEDURE spAgregar(
 	@nombre AS VARCHAR(255), 
-	@desc AS VARCHAR(255))
+	@descripcion AS VARCHAR(255))
 AS
 BEGIN
 	BEGIN TRY
 		INSERT INTO tblExamen(Nombre, Descripcion)
-		VALUES (@nombre, @desc);
+		VALUES (@nombre, @descripcion);
 		SELECT 
             0 AS codigo_retorno,
-            “Registro insertado satisfactoriamente” AS mensaje_retorno;
+            'Registro insertado satisfactoriamente' AS mensaje_retorno;
 	END TRY
 	BEGIN CATCH
 		SELECT  
